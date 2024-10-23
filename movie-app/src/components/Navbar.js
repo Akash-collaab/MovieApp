aad5791fa7519c0fd0d6bfefd32ffb970b751dd9
Akash Gupta <akashgupta84957@gmail.com>
2024-10-23 08:32:38 -0500

    Update Navbar.js

--- movie-app/src/components/Navbar.js
+++ movie-app/src/components/Navbar.js
@@ -11,20 +11,41 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
+                {/* Brand */}
                 <Link className="navbar-brand" to="/">MovieApp</Link>
-                <form className="d-flex" onSubmit={handleSearch}>
-                    <input
-                        className="form-control me-2"
-                        type="search"
-                        placeholder="Search"
-                        aria-label="Search"
-                        value={query}
-                        onChange={(e) => setQuery(e.target.value)}
-                    />
-                    <button className="btn btn-outline-success" type="submit">Search</button>
-                </form>
+                
+                {/* Navbar links */}
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav me-auto">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Popular</Link> {/* Popular movies */}
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/top-rated">Top Rated</Link> {/* Top Rated movies */}
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/upcoming">Upcoming</Link> {/* Upcoming movies */}
+                        </li>
+                    </ul>
+
+                    {/* Search bar */}
+                    <form className="d-flex" onSubmit={handleSearch}>
+                        <input
+                            className="form-control me-2"
+                            type="search"
+                            placeholder="Search"
+                            value={query}
+                            onChange={(e) => setQuery(e.target.value)}
+                        />
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                </div>
             </div>
         </nav>
     );
